a4fedc6b4cbaa7f4700856d1c2cd511cd183ac8d
Kachurovskiy Dmitriy <kachurovskiyds@gmail.com>
2024-01-08 17:22:35 +0200

    Setup typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dataSourceOptions } from './db/data.source';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+
+const DbModule = TypeOrmModule.forRoot(dataSourceOptions);
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), DbModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/db/data.source.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: false,
+  logging: false,
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/db/migrations/**/*.ts'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
